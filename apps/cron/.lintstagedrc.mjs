b39d704266f651cb6459a0c8228a4a905bad4947
Fikry Fahrezy <55774934+fikryfahrezy@users.noreply.github.com>
2025-07-28 19:16:10 +0700

    fix lintstagedrc import path on the apps

--- apps/cron/.lintstagedrc.mjs
+++ apps/cron/.lintstagedrc.mjs
@@ -1,9 +1,6 @@
-import baseConfig from '../.lintstagedrc.js'
+import baseConfig from "../../.lintstagedrc.mjs";
 
 export default {
   ...baseConfig,
-  "*.{html,css,ts,tsx,js,jsx}": [
-    "eslint --fix",
-    "prettier --write"
-  ]
-}
+  "*.{html,css,ts,tsx,js,jsx}": ["eslint --fix", "prettier --write"],
+};
